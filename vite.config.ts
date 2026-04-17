86d0cdc27c6729926eff63b00eebab855075edbb
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-04-18 01:18:05 +0530

    code changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, } from 'vite';
+// import { loadEnv } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
