493101135541cae15eb0b9f01c723dbdf299cd0f
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-04-18 12:26:43 +0530

    code changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
-import { defineConfig, } from 'vite';
-// import { loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
-import tsconfigPaths from 'vite-tsconfig-paths';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   console.log("MODE:", mode);
-
-  // const env = loadEnv(mode, process.cwd(), '');  
-  // console.log("LOADED ENV FROM VITE:", env);
 
   return {
     plugins: [
       react(),
       babel({ presets: [reactCompilerPreset()] }),
-      tsconfigPaths()
+      // tsconfigPaths(), // still useful for syncing tsconfig paths
     ],
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+
+    server: {
+      port: 5173,
+      open: true,
+    },
+
+    build: {
+      outDir: 'dist',
+      sourcemap: false,
+    },
   };
 });
